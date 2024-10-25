29726bbbcad73ffb99ca1b8b73c4a65aa8971afe
dergkijmo <322167247@qq.com>
2024-10-25 20:11:33 +0800

    wkx

--- app/Http/Controllers/WdwController.php
+++ app/Http/Controllers/WdwController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-class WdwController extends Controller
+class WdwController
 {
-    //
+
 }
---
+++ app/Models/research_stars.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models;
+
+class research_stars
+{
+
+}
